5b8ef3c1b4c5ddf1eb3bec6b51b2e85725c0644a
Marcio <marcio.soares@me.com>
2020-05-09 11:36:09 -0300

    Updates

---
+++ precache-manifest.ab38b86d801145217198eb4bd9ee446a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87750aad24aa251c50a29092c783de2f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef5ea32aa9e56780bfa3",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0bfe4b287109bdba5bfd",
+    "url": "/static/js/2.4fb76632.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4fb76632.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef5ea32aa9e56780bfa3",
+    "url": "/static/js/main.a8b88238.chunk.js"
+  },
+  {
+    "revision": "46b386583b8f5b381225",
+    "url": "/static/js/runtime-main.89f5de10.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
